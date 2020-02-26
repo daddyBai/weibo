9071d70aad4ed3417fd65013fe431dfedd749d11
daddybai <bairy1993@gmail.com>
2020-02-26 08:48:24 +0000

    中文错误提示

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
     //
     public function create()
     {
-        return view('user.create');
+        return view('users.create');
     }
 
     public function show(User $user)
