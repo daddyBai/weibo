80378d747028e6297360a6ac1908d256efd739f4
daddybai <bairy1993@gmail.com>
2020-02-26 08:28:02 +0000

    用户界面显示

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -10,6 +11,11 @@ class UsersController extends Controller
     public function create()
     {
         return view('user.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 
 }
