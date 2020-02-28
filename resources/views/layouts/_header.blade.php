b5707e2dcbc06b2a6e90da772338189be481b38d
daddyBai <133122451@qq.com>
2020-02-28 12:29:36 +0000

    管理员可删除用户

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -28,7 +28,7 @@
             @else
 
                 <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
-                <li class="nav-item" ><a class="nav-link" href="">登录</a></li>
+                <li class="nav-item" ><a class="nav-link" href="{{ route('login') }}">登录</a></li>
             @endif
         </ul>
     </div>
